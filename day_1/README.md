dcbc818faf451960f969d472b22ad8f2bcf220b3
Karthick Sabari <dskarthicksabari@gmail.com>
2021-01-12 21:15:08 +0530

    Completed the day 1 tutorial of python programming

---
+++ day_1/README.md
@@ -0,0 +1,7 @@
+In day 1 of learning python programming, I have learned
+
+- How to setup the environment
+- How to interact with the python interactive shell
+- Data types
+- Arithmetic operations
+- comments (Single line and Multi-line comments)
---
+++ day_1/helloWorld.py
@@ -0,0 +1,23 @@
+# check the python version using the command python --version
+
+print(2 + 3) # addition
+print(3 - 2) # subtraction
+print(2 * 3) # multiplication
+print(3 % 2) # modulus
+print(3 / 2) # division
+print(3 ** 3) # exponenetial
+print(3 // 2) # floor division
+
+print('Karthick')
+print('Croods')
+print('India')
+
+# checking the data types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Tesla', 'SpaceX', 'Neuralink']))
+print(type('Sabari'))
+print(type('Croods'))
+print(type('India'))
